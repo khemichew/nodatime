f053c8bbb1f6d64896e9b63e1110061fc12b4f67
Khemi <jiajinchew@gmail.com>
2024-06-14 13:07:54 +0000

    chore: target only net8.0

--- build/ci.sh
+++ build/ci.sh
@@ -8,16 +8,16 @@ cd $ROOT
 export ContinuousIntegrationBuild=true
 dotnet build -c Release src/NodaTime.sln
 dotnet test -c Release -f net8.0 src/NodaTime.Test
-dotnet test -c Release -f net8.0 src/NodaTime.Demo
-dotnet test -c Release -f net6.0 src/NodaTime.Test
+# dotnet test -c Release -f net8.0 src/NodaTime.Demo
+# dotnet test -c Release -f net6.0 src/NodaTime.Test
 
-dotnet build -c Release src/NodaTime.TzdbCompiler
-dotnet test -c Release -f net8.0 src/NodaTime.TzdbCompiler.Test
+# dotnet build -c Release src/NodaTime.TzdbCompiler
+# dotnet test -c Release -f net8.0 src/NodaTime.TzdbCompiler.Test
 
 # Publish the AOT compatibility app as an additional step to
 # find any AOT-problematic code.
-dotnet publish src/NodaTime.AotCompatibilityTestApp
+# dotnet publish src/NodaTime.AotCompatibilityTestApp
 
 # Pack the production projects, for compatibility testing.
-dotnet pack src/NodaTime
-dotnet pack src/NodaTime.Testing
+# dotnet pack src/NodaTime
+# dotnet pack src/NodaTime.Testing
